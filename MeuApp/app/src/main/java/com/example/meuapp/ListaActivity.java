82d0a6d7518a82fe551df60a9659ad3f34101ebb
Rafael Cordeiro <rafaelsilvacordeiro12@gmail.com>
2025-05-05 21:47:53 -0400

    ATL

--- MeuApp/app/src/main/java/com/example/meuapp/ListaActivity.java
+++ MeuApp/app/src/main/java/com/example/meuapp/ListaActivity.java
@@ -1,5 +1,7 @@
 package com.example.meuapp;
 
+import static com.example.meuapp.MainActivity.db;
+
 import android.os.Bundle;
 import android.widget.ListView;
 
@@ -13,6 +15,7 @@ import java.util.ArrayList;
 
 import Adapters.PessoaAdapter;
 import Model.Pessoa;
+import Model.PessoaDao;
 
 public class ListaActivity extends AppCompatActivity {
 
@@ -22,12 +25,16 @@ public class ListaActivity extends AppCompatActivity {
 
     ArrayList<Pessoa> listaPessoas;
 
+    PessoaDao pessoaDao;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_lista);
 
+        pessoaDao = db.pessoadao();
+        listaPessoas = new ArrayList<>();
         listView = findViewById(R.id.listView);
         adapter = new PessoaAdapter(getApplicationContext() , listaPessoas);
         listView.setAdapter(adapter);
